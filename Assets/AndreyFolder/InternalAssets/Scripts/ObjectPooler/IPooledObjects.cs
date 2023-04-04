c297669d79d1523a5765ff6bf948ba1d415484d8
WhiteRabbit187 <heallscream@mail.ru>
2023-04-04 23:02:58 +0300

    Removing unnecessary scripts

--- Assets/AndreyFolder/InternalAssets/Scripts/ObjectPooler/IPooledObjects.cs
+++
@@ -1,4 +0,0 @@
-public interface IPooledObjects
-{
-    StageEvent.ObjectType Type { get; }
-}
--- Assets/AndreyFolder/InternalAssets/Scripts/ScribtableObjects/EnemyData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu]
-public class EnemyData : ScriptableObject
-{
-    public string Name;
-    public GameObject Prefab;
-}
