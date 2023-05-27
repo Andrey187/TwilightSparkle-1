bf8ce84d47a0048b1542ad64787020afdbd0212d
WhiteRabbit187 <heallscream@mail.ru>
2023-05-28 01:39:25 +0300

    bug fix

--- Assets/AndreyFolder/InternalAssets/Scripts/FpsCounter.cs
+++ Assets/AndreyFolder/InternalAssets/Scripts/FpsCounter.cs
@@ -6,6 +6,10 @@ public class FpsCounter : MonoCache
     [SerializeField] private float _fps;
     [SerializeField] private TextMeshProUGUI _fpsText;
 
+    private void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
     protected override void Run()
     {
         _fps = 1.0f / Time.deltaTime;
